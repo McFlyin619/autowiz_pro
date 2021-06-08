503b4f5b609695a860320d3af33078cfe8ba70ed
OBrien <obrien.mcquade@gmail.com>
2021-06-07 20:46:10 -0700

    upgrade django

--- autowiz_pro/settings.py
+++ autowiz_pro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
